0c4a7ab90bf37be9d2f23d2e5b37ab5679c0def9
Dennis Haney <davh@voxmeter.dk>
2025-02-25 18:53:02 +0700

    fix: deployment auth sometimes has problems if configured with polly

---
+++ Catglobe.CgScript.Deployment/DeploymentAuthenticator.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Options;
+
+namespace Catglobe.CgScript.Deployment;
+
+internal partial class DeploymentAuthenticator(HttpClient httpClient, IOptions<DeploymentOptions> options)
+{
+   private string? _accessToken;
+
+   public async Task<string> GetToken(CancellationToken cancellationToken) => _accessToken ??= await AcquireToken(cancellationToken).ConfigureAwait(false);
+
+   private async Task<string> AcquireToken(CancellationToken cancellationToken)
+   {
+      var o          = options.Value;
+      httpClient.BaseAddress = options.Value.Authority;
+      var requestData = new Dictionary<string, string> {
+         {"grant_type", "client_credentials"},
+         {"client_id", o.ClientId},
+         {"client_secret", o.ClientSecret},
+         // ReSharper disable once StringLiteralTypo
+         {"scope", "scriptdeployment:w"},
+      };
+
+      var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/connect/token") { Content = new FormUrlEncodedContent(requestData), Headers = { Accept = { new("application/json") } } };
+
+      var response = await httpClient.SendAsync(requestMessage, cancellationToken).ConfigureAwait(false);
+      response.EnsureSuccessStatusCode();
+
+      var tokenResponse = await response.Content.ReadFromJsonAsync(Serializer.Default.TokenResponse, cancellationToken).ConfigureAwait(false) ?? throw new IOException("Failed to obtain authorization");
+
+      return tokenResponse.AccessToken;
+   }
+
+   private class TokenResponse
+   {
+      [JsonPropertyName("access_token")] public string AccessToken { get; set; } = null!;
+   }
+
+   [JsonSerializable(typeof(TokenResponse))]
+   [JsonSourceGenerationOptions(JsonSerializerDefaults.Web)]
+   private partial class Serializer : JsonSerializerContext;
+}
