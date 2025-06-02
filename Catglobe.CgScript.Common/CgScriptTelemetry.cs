da9dcf8569f95c58f01d6b6c3850f0c2b1d56e5d
Dennis Haney <davh@voxmeter.dk>
2025-06-02 09:58:26 +0700

    fix: missing xml comment

--- Catglobe.CgScript.Common/CgScriptTelemetry.cs
+++ Catglobe.CgScript.Common/CgScriptTelemetry.cs
@@ -7,6 +7,9 @@ namespace Catglobe.CgScript.Common;
 /// </summary>
 public static class CgScriptTelemetry
 {
+   /// <summary>
+   /// Name of source
+   /// </summary>
    public const string TelemetrySourceName = "Catglobe.CgScript";
    /// <summary>
    /// ActivitySource for CgScript
